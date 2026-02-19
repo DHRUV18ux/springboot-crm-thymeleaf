53fa08d366e1fc7148f252c6a1ed22c1de8a5894
DHRUV18UX <dhruvchourey@gmail.com>
2026-02-19 13:20:17 +0530

    Added Customer entity in model layer

--- src/main/java/com/example/SpringMvcCRMwebAppWithThymeLeaf/SpringMvcCrMwebAppWithThymeLeafApplication.java
+++ src/main/java/com/example/SpringMvcCRMwebAppWithThymeLeaf/SpringMvcCrMwebAppWithThymeLeafApplication.java
@@ -8,6 +8,7 @@ public class SpringMvcCrMwebAppWithThymeLeafApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMvcCrMwebAppWithThymeLeafApplication.class, args);
+
 	}
 
 }
