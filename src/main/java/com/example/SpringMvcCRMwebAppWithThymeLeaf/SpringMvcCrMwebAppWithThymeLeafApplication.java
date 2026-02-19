0557fb9e5f7ea002625cd1b0e6ac210cae1e2313
DHRUV18UX <dhruvchourey@gmail.com>
2026-02-19 12:43:41 +0530

    Initial Spring Boot project setup with Thymeleaf

---
+++ src/main/java/com/example/SpringMvcCRMwebAppWithThymeLeaf/SpringMvcCrMwebAppWithThymeLeafApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringMvcCRMwebAppWithThymeLeaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcCrMwebAppWithThymeLeafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcCrMwebAppWithThymeLeafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringMvcCRMwebAppWithThymeLeaf/SpringMvcCrMwebAppWithThymeLeafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringMvcCRMwebAppWithThymeLeaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcCrMwebAppWithThymeLeafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
